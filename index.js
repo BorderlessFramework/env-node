056caa695672efa3a0f4b2402c476de01a705220
Sergey Chernyshev <sergey.chernyshev@gmail.com>
2022-07-29 22:20:40 -0400

    Added build command

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import { fileURLToPath } from "node:url";
 
-export default function () {
-  console.log("env-node module reporting for duty");
-
+export default function (config) {
   return {
     src: "src/",
     boilerplate: fileURLToPath(new URL("boilerplate/", import.meta.url)),
+    buildCommand: "npm run build",
+    buildOutputFolder: "build/",
   };
 }
